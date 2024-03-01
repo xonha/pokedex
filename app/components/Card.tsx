be2af0ffc2658ba2eeac27a2dbbffc1129f5aa27
henrique <henrique@xonha.space>
2024-03-01 10:18:13 -0300

    fix: font and id layout

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,4 +1,4 @@
-import { Pokemon, TypeColors } from "@/interfaces/pokemon";
+import { Pokemon } from "@/interfaces/pokemon";
 import Image from "next/image";
 
 export interface PokemonProps {
@@ -16,23 +16,23 @@ export function Card({ pokemon }: PokemonProps) {
         width="200"
       />
       <div className="text-center">
-        <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-          #{pokemon.id}
-        </p>
         <h3 className="text-xl font-bold">
           {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+          <span className="pl-2 font-medium text-gray-500 dark:text-gray-400">
+            #{pokemon.id}
+          </span>
         </h3>
         <div className="flex flex-row gap-2 justify-center">
           {pokemon.types.map((type) => (
             <div
               key={pokemon.id + type.type.name}
-              className={`bg-${type.type.name} w-fit px-1 rounded-md text-white`}
+              className={`bg-${type.type.name} w-fit px-1 rounded-md text-white font-bold`}
             >
               {type.type.name}
             </div>
           ))}
         </div>
-        <p className="">Weight: {pokemon.weight}</p>
+        <p className="">Weight: {pokemon.weight / 10} kg</p>
       </div>
     </div>
   );
